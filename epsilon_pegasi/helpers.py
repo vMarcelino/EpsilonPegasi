a9ff5e88ff1d29f12c822e5a6cf37893718f946c
Victor Marcelino <victor.fmarcelino@gmail.com>
2019-10-12 14:39:49 -0300

    improved enforced_dataclass

--- epsilon_pegasi/helpers.py
+++ epsilon_pegasi/helpers.py
@@ -1,44 +1,100 @@
 from dataclasses import dataclass
 from functools import wraps
-from typing import List, Union
+from typing import List, Union, Dict
 
 
 def _try_eval(code: str):
     try:
         return eval(code)
     except:
-        return None
+        return lambda x: x
 
 
-def _enforced_dataclass(cls, exceptions: List[Union[type, str]], blacklist: bool, debug: bool = False):
+def _enforced_dataclass(cls,
+                        exceptions: List[Union[type, str]],
+                        replaces: Dict[str, callable],
+                        blacklist: bool,
+                        debug: bool = False):
+    if exceptions is None:
+        exceptions = []
+    if replaces is None:
+        replaces = {}
     dataclass(cls)
     init = cls.__init__
-    _annotations = init.__annotations__
-    annotations = {}
-    for k, v in _annotations.items():
-        if callable(v):
-            annotations[k] = v
-        else:
-            ev = _try_eval(v)
-            if callable(ev):
-                annotations[k] = ev
-
-    for i, e in enumerate(exceptions):
-        if type(e) is str:
-            exceptions[i] = eval(e)
+    annotations = init.__annotations__
+    exceptions_str = [r if type(r) is str else r.__name__ for r in exceptions]
+    replaces_str = {t if type(t) is str else t.__name__: r for t, r in replaces.items()}
 
     @wraps(init)
     def __init__(*args, **kwargs):
         # converts args to kwargs
         kwargs.update(zip(init.__code__.co_varnames, args))
 
+        def enforce_type(kwarg):
+            value = kwargs[kwarg]
+            enforce_func = annotations[kwarg]
+            if type(enforce_func) is str:
+                if enforce_func in replaces_str:
+                    enforce_func = replaces_str[enforce_func]
+                    if debug: print('using custom enforce function:', enforce_func)
+                else:
+                    enforce_func = _try_eval(enforce_func)
+            else:
+                if enforce_func in replaces:
+                    enforce_func = replaces[enforce_func]
+                    if debug: print('using custom enforce function:', enforce_func)
+                elif enforce_func.__name__ in replaces_str:
+                    enforce_func = replaces_str[enforce_func.__name__]
+                    if debug: print('using custom enforce function:', enforce_func)
+
+            return enforce_func(value)
+
+        def type_check(kwarg):
+            mapper = annotations[kwarg]
+            value = kwargs[kwarg]
+            if type(mapper) is str:
+                #check by type name
+                if type(value).__name__ != mapper:
+                    # enforce type
+                    kwargs[kwarg] = enforce_type(kwarg)
+                    if debug: print('enforced to', repr(kwargs[kwarg]))
+
+            else:
+                # check by type
+                if type(value) is not mapper:
+                    #enforce type
+                    kwargs[kwarg] = enforce_type(kwarg)
+                    if debug: print('enforced to', repr(kwargs[kwarg]))
+
         # enforces types
-        for kwarg in kwargs:
+        for kwarg, value in kwargs.items():
             if kwarg in annotations:
-                if debug: print('found', kwarg, 'as', kwargs[kwarg])
-                if (annotations[kwarg] in exceptions) ^ blacklist:
-                    kwargs[kwarg] = annotations[kwarg](kwargs[kwarg])
-                    if debug: print('enforcing to ', annotations[kwarg])
+                if debug: print('found', kwarg, 'as', repr(value), 'of type', type(value))
+                mapper = annotations[kwarg]
+                is_exc = False
+                if type(mapper) is str:
+                    is_exc = mapper in exceptions_str
+                else:
+                    is_exc = mapper in exceptions or mapper.__name__ in exceptions_str
+
+                if is_exc:
+                    if blacklist:
+                        # do not check
+                        if debug: print('not enforcing')
+                        pass
+                    else:
+                        # check
+                        type_check(kwarg)
+                        pass
+                else:
+                    if blacklist:
+                        # check
+                        type_check(kwarg)
+                        pass
+                    else:
+                        # do not check
+                        if debug: print('not enforcing')
+                        pass
 
         return init(**kwargs)
 
@@ -49,9 +105,10 @@ def _enforced_dataclass(cls, exceptions: List[Union[type, str]], blacklist: bool
 def enforced_dataclass(cls=None,
                        *,
                        exceptions: List[Union[type, str]] = None,
-                       blacklist: bool = False,
+                       replaces: Dict[str, callable] = None,
+                       blacklist: bool = True,
                        debug: bool = False):
     if cls:
-        return _enforced_dataclass(cls, [], True, debug)
+        return _enforced_dataclass(cls, [], {}, True, debug)
     else:
-        return lambda _cls: _enforced_dataclass(_cls, exceptions, blacklist, debug)
+        return lambda _cls: _enforced_dataclass(_cls, exceptions, replaces, blacklist, debug)
